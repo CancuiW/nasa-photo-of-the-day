5a91a881709393a2909fff809af520a2339442f2
CancuiW <can.cui@wayne.edu>
2023-07-05 18:50:41 -0700

    project in M3S6

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React,{useState,useEffect}from "react";
 import "./App.css";
+import axios from "axios";
+import NasaPhoto from "./components/NasaPhoto";
+
 
 function App() {
+  const [originData,setOriginData]=useState()
+
+  useEffect(()=>{
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+          .then (res=>{
+            console.log(res.data)
+            setOriginData(res.data)
+          })
+          .catch(err=>{
+            console.error(err)
+          })
+
+  },[])
+
+
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+     {originData && <NasaPhoto datas={originData} />}
     </div>
   );
 }
 
 export default App;
+
